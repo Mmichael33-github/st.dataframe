b8f0938335b927ed9f27510731fb14c6d58d5b09
Mmichael33-github <114730461+Mmichael33-github@users.noreply.github.com>
2022-12-10 23:37:45 +0200

    Update multiselect_symptoms.py

--- multiselect_symptoms.py
+++ multiselect_symptoms.py
@@ -33,10 +33,10 @@ user_side_effects = side_effects.query('drug in @user_drugs').sort_values(
 #st.dataframe(user_side_effects, use_container_width = True)
 
 
-st.write('The drugs you entered are: ', 
-         user_side_effects['side_effect'].unique().tolist())
+#st.write('The drugs you entered are: ', 
+#         user_side_effects['side_effect'].unique().tolist())
 
 
-#st.dataframe(user_side_effects.unique(), use_container_width = True)
+st.dataframe(user_side_effects['side_effect'].unique(), use_container_width = True)
 
 
